6077874ad3404b9395ab2e9d74634ede7426f9ac
coreIAM <manager@ecstatic-curie.hivelocitydns.com>
2026-05-03 15:25:26 +0000

    fix: include actions in build and ship dist

---
+++ javascript/dist/nextjs/actions.d.ts
@@ -0,0 +1,8 @@
+export interface AuthResult {
+    ok: boolean;
+    status: number;
+    data: any;
+}
+export declare function loginActionSdk(identifier: string, password: string): Promise<AuthResult>;
+export declare function registerActionSdk(userData: any): Promise<AuthResult>;
+export declare function logoutActionSdk(): Promise<AuthResult>;
