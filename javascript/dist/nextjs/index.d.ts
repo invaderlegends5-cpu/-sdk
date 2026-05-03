ccbd2f5eb5f849b8ed2577b3a63b7e09a82daebb
coreIAM <manager@ecstatic-curie.hivelocitydns.com>
2026-05-03 11:43:26 +0000

    chore: remove tracked dist folder from github

--- javascript/dist/nextjs/index.d.ts
+++
@@ -1,7 +0,0 @@
-import { NextRequest, NextResponse } from 'next/server';
-import { CoreIAM } from '../core';
-export declare class NextHandlers {
-    private iam;
-    constructor(iam: CoreIAM);
-    handleProxy(req: NextRequest, internalPath: string): Promise<NextResponse<any>>;
-}
